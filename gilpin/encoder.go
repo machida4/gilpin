c9a048abb3a4ee05c1aef14a25fe43f246249de3
machida <machida@kmc.gr.jp>
2021-08-14 01:12:57 +0900

    create encoder

--- gilpin/encoder.go
+++ gilpin/encoder.go
@@ -1,1 +1,18 @@
 package gilpin
+
+import (
+	"bytes"
+	"io"
+)
+
+type Encoder struct {
+	buffer *bytes.Buffer
+
+	ImageData *ImageData
+}
+
+func NewEncoder(r io.Writer) *Encoder {
+
+}
+
+func (e *Encoder) Encode()
